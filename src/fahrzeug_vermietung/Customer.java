563a60e343b7544a6c3e9407a984efb56833191d
oskar <zecosc16@htlkaindorf.at>
2019-05-01 11:42:42 +0200

    created Customer

---
+++ src/fahrzeug_vermietung/Customer.java
@@ -0,0 +1,19 @@
+package fahrzeug_vermietung;
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author oskar
+ */
+public class Customer {
+    private String name;
+    private LocalDate gebDat;
+    private String telNum;
+}
