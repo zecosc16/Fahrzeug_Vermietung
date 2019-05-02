9298ebce83879318deeb487ec06de2cc6c1af8b2
oskar <zecosc16@htlkaindorf.at>
2019-05-02 08:17:40 +0200

    created VehicleDialog

--- src/fahrzeug_vermietung/Customer.java
+++ src/fahrzeug_vermietung/Customer.java
@@ -16,4 +16,38 @@ public class Customer {
     private String name;
     private LocalDate gebDat;
     private String telNum;
+    private double money;
+    private int custID;
+
+    public Customer(String name, LocalDate gebDat, String telNum, double money,int custID) {
+        this.name = name;
+        this.gebDat = gebDat;
+        this.telNum = telNum;
+        this.money = money;
+        this.custID=custID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getGebDat() {
+        return gebDat;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+    
+    
+    
+    
+    public void pay(double amount){
+        money+=amount;
+    }
+    
 }
